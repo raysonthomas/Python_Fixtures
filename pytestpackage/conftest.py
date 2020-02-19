a4b61000ba83138c2305f8a118203542c0cde3e9
Rayson Thomas <Rayson.Thomas@uk.bosch.com>
2020-02-19 14:43:46 +0000

    Fixtures tests

---
+++ pytestpackage/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+
+
+@pytest.yield_fixture()
+def setUp():
+    print("Running method level setup")
+    yield
+    print("Running method level teardown")
+
+@pytest.yield_fixture(scope="class")
+def oneTimeSetUp(request, browser):
+    print("Running one time setup")
+    if browser == 'firefox':
+        value = 10
+        print("Running tests on FF ")
+    else:
+        value = 20
+        print("Running tests on Chrome")
+
+    if request.cls is not None:
+        request.cls.value = value
+    yield value
+    print("Running one time tearDown")
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+    parser.addoption("--osType",help="Type of operating system")
+
+@pytest.fixture(scope="session")
+def browser(request):
+    return request.config.getoption("--browser")
+
+@pytest.fixture(scope="session")
+def osType(request):
+    return request.config.getoption("--osType")
